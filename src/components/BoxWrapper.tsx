7e6691a308015ba8c3f90dc74c61ab8823dbae04
Avi <avidrucker@gmail.com>
2020-03-27 17:41:55 -0400

    fix relocate wrapper style to reduce code dup

--- src/components/BoxWrapper.tsx
+++ src/components/BoxWrapper.tsx
@@ -2,6 +2,6 @@ import React  from 'react';
 
 export const BoxWrapper = 
 	(props: any) =>
-	<div className={props.className}>
+	<div className={props.className + " pa2 ba b--black mb2 mr2 dib"}>
 		{props.children}
 	</div>;
