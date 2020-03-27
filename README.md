a43efcb0016fae881bb7510b3730c50d220dd5ec
Avi <avidrucker@gmail.com>
2020-03-27 16:06:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Autofocus-UI
+
+This is a working template of atomic components built for the AutoFocus-Exp to-do list app.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      <header>
+				<h1>AutoFocus</h1>
+			</header>
+			<section>
+				Welcome to AutoFocus!
+			</section>
+    </main>
+  );
+}
+
+export default App;
