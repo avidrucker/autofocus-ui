3696318f0cd5facf04344f8c3955fcbaed138842
Avi <avidrucker@gmail.com>
2020-03-27 17:43:49 -0400

    fix improve formatting

--- src/components/InputNewOrg.tsx
+++ src/components/InputNewOrg.tsx
@@ -1,13 +1,19 @@
 import React, { Fragment } from 'react';
 import { HeaderMol } from './HeaderMol';
 
-export default function InputNewOrg () {
+export default function InputNewOrg (props: any) {
 	return (
 		<Fragment>
-			<HeaderMol h2="Import New Screen" backBtn={true} />
+			<HeaderMol
+				h2={`Import New Screen ${props.designId}`}
+				backBtn={true}
+			/>
 			<section>
 				<p>Enter a new todo:</p>
-				<textarea style={{width: '100%'}} placeholder="eg. Wash the dishes" />
+				<textarea
+					style={{width: '100%'}}
+					placeholder="eg. Wash the dishes"
+				/>
 				<button disabled>Confirm</button>
 			</section>
 		</Fragment>
