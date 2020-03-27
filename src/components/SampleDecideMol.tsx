8f845ab11ece4f303ef137405726be1eee2d727f
Avi <avidrucker@gmail.com>
2020-03-27 17:00:14 -0400

    feat dummy data components

---
+++ src/components/SampleDecideMol.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const SampleDecideMol = () =>
+	<section>
+		<p>
+			<span>Do you want to </span>
+			<strong>wash the dishes</strong>
+			<span> more than </span>
+			<strong>finish trig problem sets for today</strong>
+			<span>?</span>
+		</p>
+		<button>No</button>
+		<button>Yes</button>
+	</section>
---
+++ src/components/SampleHomeMol.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const SampleHomeMol = () =>
+	<section>
+		<ul>
+			<li className="unmarked">Finish trig problem sets for today</li>
+			<li className="unmarked">Wash the dishes</li>
+			<li className="unmarked">Pack for trip tomorrow</li>
+		</ul>
+		<button>Decide what to do</button>
+		<button>Add new todo</button>
+	</section>
