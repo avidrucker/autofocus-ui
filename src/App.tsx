19c7501543316e9b58c0b9b8f856e5ef6dcd167b
Avi <avidrucker@gmail.com>
2020-03-27 17:00:37 -0400

    feat display screen org components as list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import './App.css';
 
+import { BoxWrapper } from './components/BoxWrapper';
+
+import WelcomeOrg from './components/WelcomeOrg';
+import InputNewOrg from './components/InputNewOrg';
+import HomeOrg from './components/HomeOrg';
+import DecideOrg from './components/DecideOrg';
+
 function App() {
   return (
-    <main className="App">
-      <header>
-				<h1>AutoFocus</h1>
-			</header>
-			<section>
-				Welcome to AutoFocus!
-			</section>
+    <main className="border-box">
+			<BoxWrapper className="ba b--black mb2">
+				<WelcomeOrg />
+			</BoxWrapper>
+
+			<BoxWrapper className="ba b--black mb2">
+				<InputNewOrg />
+			</BoxWrapper>
+
+			<BoxWrapper className="ba b--black mb2">
+				<HomeOrg />
+			</BoxWrapper>
+
+			<BoxWrapper className="ba b--black mb2">
+				<DecideOrg />
+			</BoxWrapper>
     </main>
   );
 }
