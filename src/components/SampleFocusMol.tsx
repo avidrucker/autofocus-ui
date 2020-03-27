23e47d2861e00b6084e16e8f22b45a09d45a73a9
Avi <avidrucker@gmail.com>
2020-03-27 17:44:17 -0400

    feat dummy screen components

---
+++ src/components/SampleFocusMol.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const SampleFocusMol = () =>
+	<section>
+		<p>
+			<span>You are focusing on </span>
+			<strong>finish my trig problem sets for today</strong>
+		</p>
+		<button>Return home without saving</button>
+		<button>Stopping with work left</button>
+		<button>Task is 100% complete</button>
+	</section>
---
+++ src/components/SampleHomeMol2.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const SampleHomeMol2 = () =>
+	<section>
+		<ul>
+			<li className="marked">Finish trig problem sets for today</li>
+			<li className="unmarked">Wash the dishes</li>
+			<li className="marked">Pack for trip tomorrow</li>
+		</ul>
+		<button>Enter Focus Mode</button>
+		<button>Decide what to do</button>
+		<button>Add new todo</button>
+	</section>
