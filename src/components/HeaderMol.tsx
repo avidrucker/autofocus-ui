e3efc05cf2d32f4e54615a1141d1eb3b7bf25b2c
Avi <avidrucker@gmail.com>
2020-03-28 22:41:58 -0400

    update replace dummy button with working button

--- src/components/HeaderMol.tsx
+++ src/components/HeaderMol.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import BackBtnAtom from './BackBtnAtom';
 
 export const HeaderMol = (props: any) =>
 	<header>
 		{!!props.backBtn &&
-			<button>Back</button>
+			<BackBtnAtom text="Back" />
 		}
 		<h1 className={!!props.backBtn ? "dib" : ""}>
 			AutoFocus
