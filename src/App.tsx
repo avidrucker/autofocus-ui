8b43ef6f387a4bddf61e19661e21772ac69d7856
Avi <avidrucker@gmail.com>
2020-03-28 22:40:54 -0400

    feat implement routes, add at a glance master view

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { BoxWrapper } from './components/BoxWrapper';
+import { Switch, Route } from 'react-router-dom';
 
 import WelcomeOrg from './components/WelcomeOrg';
 import InputNewOrg from './components/InputNewOrg';
@@ -10,22 +10,38 @@ import DecideOrg from './components/DecideOrg';
 import HomeMarkedOrg from './components/HomeMarkedOrg';
 import FocusOrg from './components/FocusOrg';
 import HomeWithDoneOrg from './components/HomeWithDoneOrg';
+import AllScreensOrg from './components/AllScreensOrg';
 
 function App() {
-	let compList = [
-		<WelcomeOrg designId="001" />,
-		<InputNewOrg designId="002" />,
-		<HomeOrg designId="004" />,
-		<DecideOrg designId="005" />,
-		<HomeMarkedOrg designId="006" />,
-		<FocusOrg designId="007" />,
-		<HomeWithDoneOrg designId="008" />
-	];
-	compList = compList.map((x, i) => <BoxWrapper key={i}>{x}</BoxWrapper>);
-
   return (
     <main className="border-box">
-			{compList}
+			<Switch>
+				<Route
+					exact path='/input-new'
+					render={(props) => <InputNewOrg {...props} designId="002" />} />
+				<Route
+					exact path='/home'
+					render={(props) => <HomeOrg {...props} designId="004" />} />
+				<Route
+					exact path='/decide'
+					render={(props) => <DecideOrg {...props} designId="005" />} />
+				<Route
+					exact path='/home-marked'
+					render={(props) => 
+						<HomeMarkedOrg {...props} designId="006" />} />
+				<Route
+					exact path='/focus'
+					render={(props) => <FocusOrg {...props} designId="007" />} />
+				<Route
+					exact path='/home-with-done'
+					render={(props) => <HomeWithDoneOrg {...props} designId="007" />} />
+				<Route
+					exact path='/welcome'
+					render={(props) => <WelcomeOrg {...props} designId="001" />} />
+				<Route
+					path='/'
+					component={AllScreensOrg} />
+			</Switch>
     </main>
   );
 }
