064010909198c639c0cf78b393f62a0eaea9406d
Avi <avidrucker@gmail.com>
2020-03-31 11:57:11 -0400

    feat add links to btns to wire up demo

--- src/components/SampleHomeMol3.tsx
+++ src/components/SampleHomeMol3.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import { BoxWrapper } from './BoxWrapper';
+
+import { Link } from 'react-router-dom';
+
 export const SampleHomeMol3 = () =>
 	<section>
 		<ul>
@@ -7,7 +11,13 @@ export const SampleHomeMol3 = () =>
 			<li className="unmarked">Wash the dishes</li>
 			<li className="marked">Pack for trip tomorrow</li>
 		</ul>
-		<button>Enter Focus Mode</button>
-		<button>Decide what to do</button>
-		<button>Add new todo</button>
+		<BoxWrapper>
+			<button>Enter Focus Mode</button>
+			<Link to="/decide">
+				<button>Decide what to do</button>
+			</Link>
+			<Link to="/input-new">
+				<button>Add new todo</button>
+			</Link>
+		</BoxWrapper>
 	</section>
