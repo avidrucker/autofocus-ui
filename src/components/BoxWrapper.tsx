d6b23e9de20f27483efa3c36776718bda53cac4c
Avi <avidrucker@gmail.com>
2020-03-31 11:21:44 -0400

    fix apply minor style adjust

--- src/components/BoxWrapper.tsx
+++ src/components/BoxWrapper.tsx
@@ -2,6 +2,6 @@ import React  from 'react';
 
 export const BoxWrapper = 
 	(props: any) =>
-	<div className={props.className + " pa2 ba b--black mb2 mr2"}>
+	<div className={props.className + " pa2 ba b--black"}>
 		{props.children}
 	</div>;
