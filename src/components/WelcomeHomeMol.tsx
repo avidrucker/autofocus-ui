255ef23b8a34f0047c1aaa008e7fdc035046ab60
Avi <avidrucker@gmail.com>
2020-03-31 11:22:02 -0400

    update add wrapper to btn link

--- src/components/WelcomeHomeMol.tsx
+++ src/components/WelcomeHomeMol.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { BoxWrapper } from './BoxWrapper';
+
 export const WelcomeHomeMol = () =>
 	<section>
 		<p>Welcome to AutoFocus!</p>
 		<p>Tap the button below to add a new todo item.</p>
-		<Link to="/input-new">
-			<button>Add new todo</button>
-		</Link>
+		<BoxWrapper>
+			<Link to="/input-new">
+				<button>Add new todo</button>
+			</Link>
+		</BoxWrapper>
 	</section>
