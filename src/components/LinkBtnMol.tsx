6d54e6b05924066ce7d929d4ffa682519d316483
Avi <avidrucker@gmail.com>
2020-04-25 13:01:36 -0400

    feat apply theming

--- src/components/LinkBtnMol.tsx
+++ src/components/LinkBtnMol.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { styles } from './theming';
 
 export const LinkBtnMol = (props: any) =>
 		<Link to={props.to}>
 			<button
 				disabled={props.disabled}
-				className={props.className + " w-100"}
+				className={props.className + ` w-100 pa2 mt3 ${styles.button}`}
 				>
 				{props.text}
 			</button>
