8ce09786d3cce69a0a0171b971cb8eea90934354
Avi <avidrucker@gmail.com>
2020-04-25 13:00:01 -0400

    feat apply theming, vertically center header

--- src/components/HeaderMol.tsx
+++ src/components/HeaderMol.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import BackBtnAtom from './BackBtnAtom';
 import { HeaderWrapper } from './HeaderWrapper';
+import { styles } from './theming';
 
 export const HeaderMol = (props: any) =>
-	<header className="relative w-100 white">
-		<HeaderWrapper>
+	<header className="pa3 pt4 h4 bg-blue relative w-100 white">
+		<HeaderWrapper className="flex flex-row items-center">
 			{!!props.backBtn &&
-				<BackBtnAtom text="Back" className="mr2" />
+				<BackBtnAtom text="Back" className={`pa2 mr2 ${styles.backButton}`} />
 			}
-			<h1 className={!!props.backBtn ? "dib" : ""}>
+			<h1 className={!!props.backBtn ? `dib pl3 ma0 ${styles.header}` : `tc center ma0 ${styles.header}`}>
 				AutoFocus
 			</h1>
-			<div className="absolute top-0 right-0 o-70 pa1 w-40">
+			<div className="dn absolute top-0 right-0 o-70 w-40">
 				<label>{props.h2}</label>
 			</div>
 		</HeaderWrapper>
